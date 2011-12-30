ad9aa975b2cb64f57836b30335c1efd6a7c30301
Goran Jovic <goranjovic@gmail.com>
2011-12-30 20:43:13 +0100

    showing result in a notification

--- chrome/chromeclojure.js
+++ chrome/chromeclojure.js
@@ -1,14 +1,23 @@
 
+function sendMessage(response){
+	var notification = webkitNotifications.createNotification(
+	  'icon-small.png',
+	  response.error ? 'Error' : 'Result',
+	  response.result
+	);
+	notification.show();
+}
 
 function handleEvalClojureClick(info, tab){
+
 
 	 $.ajax({
 		url: 'http://chromeclojure.com/eval',
 		type: 'POST',
-		dataType: 'text',
+		dataType: 'json',
                 data: {source: info.selectionText},
 		success: function(response){
-			alert(response);
+			sendMessage(response);
 		},
                 error: function(){
 			alert('Error!!1');
