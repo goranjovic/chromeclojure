215b141999e2f231ec917ae8a0b2d65cf1fbb867
Goran Jovic <goranjovic@gmail.com>
2012-01-08 15:20:40 +0100

    options page and ability to choose response method

--- chrome/chromeclojure.js
+++ chrome/chromeclojure.js
@@ -1,18 +1,36 @@
 
+var messageSenders = {
+
+	'notification' : function(response){
+		var notification = webkitNotifications.createNotification(
+		  'icon-small.png',
+		  response.error ? 'Error' : 'Result',
+		  response.result
+		);
+		notification.show();
+	},
+
+	'alert' : function(response){
+		alert((response.error ? 'Error' : 'Result') + ':\n' + response.result);
+	}
+
+};
+
+
 function sendMessage(response){
-	var notification = webkitNotifications.createNotification(
-	  'icon-small.png',
-	  response.error ? 'Error' : 'Result',
-	  response.result
-	);
-	notification.show();
+	var responseMethod = localStorage['responseMethod'];
+	if(responseMethod == undefined || responseMethod == null || responseMethod == ''){
+		responseMethod = 'notification';
+	}
+	var sender = messageSenders[responseMethod];
+	sender(response);
 }
 
 function handleEvalClojureClick(info, tab){
 
 
 	 $.ajax({
-		url: 'http://chromeclojure.com/eval',
+		url: 'http://chromeclojure.com:8081/eval',
 		type: 'POST',
 		dataType: 'json',
                 data: {source: info.selectionText},
