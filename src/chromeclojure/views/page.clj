0ccd57d5ea9959b48ea2e9b944627e1cea0c09dd
Goran Jovic <goranjovic@gmail.com>
2012-01-11 21:29:44 +0100

    a basic homepage

---
+++ src/chromeclojure/views/page.clj
@@ -0,0 +1,17 @@
+(ns chromeclojure.views.page
+  (:use [noir.core :only [defpage]]
+        [chromeclojure.models.eval :only [eval-request]])
+  (:use hiccup.core)
+  (:use hiccup.page-helpers)
+  (:require [noir.response :as resp]))
+
+(defpage "/" {}
+ (html5
+  (include-css "/css/chromeclojure.css")
+   [:div#github-banner [:a {:href "https://github.com/goranjovic/chromeclojure" :alt "Fork chromeclojure on Github!"}]]
+    [:h1 "Clojure Extension for Chrome"]
+    [:h2 "Adds an option to the context menu which allows you to eval selected Clojure code."]
+    [:p "Like Clojure and often read blogs about it? Often have to start up a REPL and copy & paste a snippet just to see how will it evaluate?"]
+    [:p "Worry no more! Install this Chrome extension and you will have another option in your browser's context menu - Eval as Clojure. Select a piece of Clojure code, right click it and choose this option and you will instantly get a notification with evaluation results."]
+    [:p (link-to "https://chrome.google.com/webstore/detail/dkhaobmljgohccicjemmbacpooaacgeo" "Install") " this extension and try it out yourself!"]
+   ))
