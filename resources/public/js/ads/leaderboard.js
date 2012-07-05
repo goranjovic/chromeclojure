c7e3b51105ad5f42249f0e9b95db6a57834d0832
Goran Jovic <goranjovic@gmail.com>
2012-07-05 14:00:50 +0200

    sticky footer, privacy policy & adsense

---
+++ resources/public/js/ads/leaderboard.js
@@ -0,0 +1,11 @@
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-1048742787187504";
+/* chromeclojure-leaderboard */
+google_ad_slot = "4636959266";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
